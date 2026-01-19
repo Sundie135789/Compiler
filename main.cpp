764f17d1a01f614863c96ddb80dda77667bd126e
Sundie135789 <bhandarimudit5@gmail.com>
2026-01-19 20:11:52 +0530

    Tokenization function + taking input.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 #include <sstream>
 
 std::string output;
+std::string* vecToArr(const std::vector<std::string>& v){
+  std::string* arr = new std::string[v.size()];
+  for(size_t i = 0;i < v.size();i++){
+    arr[i] = v[i];
+  }
+  return arr;
+}
+std::vector<std::string> tokenize(std::string line){
+  std::stringstream ss(line);
+  std::string word;
+  std::vector<std::string> tokens;
+  while(ss >> word){
+    tokens.push_back(word);
+  }
+  return tokens;
+} 
+void convert(std::string line){
+  
+}
 
 int main(){
   std::string line;
